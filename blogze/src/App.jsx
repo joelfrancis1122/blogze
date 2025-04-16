154f47fa5aef9077996f2879003931db83dc47c2
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-16 18:20:15 +0530

    checkout practice

--- blogze/src/App.jsx
+++ blogze/src/App.jsx
@@ -18,6 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        
+        <>
+        <h1></h1>
+        </>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
