f30d8793fd34fd9ed7a5ad5dc8a1ea2e1cf952e6
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-18 16:30:55 +0530

    space

--- blogze/src/App.jsx
+++ blogze/src/App.jsx
@@ -16,9 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        
         <>
         <h1></h1>
         </>
