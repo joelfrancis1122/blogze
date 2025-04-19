4bc186b87b35e70878f5e39106763b484cf800bd
JOEL FRANCIS <joelfrancis9398@gmail.com>
2025-04-19 20:31:52 +0530

    space

--- blogze/src/App.jsx
+++ blogze/src/App.jsx
@@ -18,19 +18,13 @@ function App() {
       </div>
       <div className="card">
         <>
-        <h1></h1>
         </>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-  
-  
-
-
-
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
